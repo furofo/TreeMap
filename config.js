fc3980bfd8d859f9c1f7fb8b14df56c6bd799c9f
furofo <chriscellphonerepair@gmail.com>
2020-04-20 19:21:39 -0400

    pulled data from api

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
-console.log("hello World");
+$(document).ready(function() {
+    fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json')
+    .then(response => response.json())
+    .then(data => {
+       let json = JSON.parse(JSON.stringify(data));
+        console.log(json);
+    });
+});
