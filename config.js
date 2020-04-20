3254a62df47a322eb6b0f0cb61540c0fd7e0c7db
furofo <chriscellphonerepair@gmail.com>
2020-04-20 19:35:53 -0400

    starting logic for svg element

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
+let makeTreeMap = function (json) {
+    let margin = {top: 10, right: 10, bottom: 10, left: 10}
+    let svg = d3.select(treeSvg);
+    width = 445 - margin.left - margin.right;
+    height = 445 - margin.bottom - margin.bottom;
+}
+
+
 $(document).ready(function() {
     fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/video-game-sales-data.json')
     .then(response => response.json())
