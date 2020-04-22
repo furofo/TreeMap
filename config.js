fca65a4d7ac71f0c3518f33668859b767b502b08
furofo <chriscellphonerepair@gmail.com>
2020-04-21 20:24:36 -0400

    added logic for data to display tree map

--- config.js
+++ config.js
@@ -1,8 +1,30 @@
 let makeTreeMap = function (json) {
     let margin = {top: 10, right: 10, bottom: 10, left: 10}
-    let svg = d3.select(treeSvg);
-    width = 445 - margin.left - margin.right;
-    height = 445 - margin.bottom - margin.bottom;
+    let width = 445 - margin.left - margin.right;
+    let  height = 445 - margin.bottom - margin.bottom;
+    let svg = d3.select('#treeSvg')
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height - margin.top - margin.bottom)
+             .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    let root = d3.hierarchy(json).sum(function(d) {return d.value});
+
+    d3.treemap()
+    .size([width, height])
+    .padding(2)
+    (root);
+
+    svg.selectAll("rect")
+        .data(root.leaves())
+        .enter()
+        .append("rect")
+        .attr("x", function(d) {return d.x0})
+        .attr("y", function(d) {return d.y0})
+        .attr('width', function (d) { return d.x1 - d.x0; })
+        .attr('height', function (d) { return d.y1 - d.y0; })
+        .style("stroke", "black")
+        .style("fill", "slateblue");
 }
 
 
@@ -11,6 +33,6 @@ $(document).ready(function() {
     .then(response => response.json())
     .then(data => {
        let json = JSON.parse(JSON.stringify(data));
-        console.log(json);
+       makeTreeMap(json);
     });
 });
