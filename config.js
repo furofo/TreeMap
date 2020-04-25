7f7aaeff25d12bda4260e9df430c11998efd3815
furofo <chriscellphonerepair@gmail.com>
2020-04-24 20:22:10 -0400

    changed aspect ratio and padd for visual

--- config.js
+++ config.js
@@ -1,30 +1,52 @@
-let makeTreeMap = function (json) {
-    let margin = {top: 10, right: 10, bottom: 10, left: 10}
-    let width = 445 - margin.left - margin.right;
-    let  height = 445 - margin.bottom - margin.bottom;
-    let svg = d3.select('#treeSvg')
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height - margin.top - margin.bottom)
-             .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+let makeTreeMap = function (json) { // this is function to actually make tree map
+    //let margin = {top: 10, right: 10, bottom: 10, left: 10} // just  a convention for adding margin may remove this later
+    let width = 960;// gets width without the marings
+    let  height = 570;// height without the margins 
+    let svg = d3.select('#treeSvg') // selects my tree svg node from html and assigns it to variable svg
+                .attr("width", width) // gives it complete width account for margin later
+                .attr("height", height) // gives it complete height account for margin later
+             
+             
+             
+    //let g = svg.append("g") // this is g elmeent that I can use for magin
+              // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    let root = d3.hierarchy(json).sum(function(d) {return d.value});
+    let root = d3.hierarchy(json).sum(function(d) {
+       // console.log("this is d " + d);
+       // console.log(d);
+        return d.value
+    }).sort(function(a, b) { return b.height - a.height || b.value - a.value; });
+    
 
-    d3.treemap()
+    let treeMap = d3.treemap()
     .size([width, height])
-    .padding(2)
-    (root);
+    .padding(1);
+
+    treeMap(root);
 
     svg.selectAll("rect")
         .data(root.leaves())
         .enter()
         .append("rect")
-        .attr("x", function(d) {return d.x0})
+        .attr("x", function(d) {
+            return d.x0})
         .attr("y", function(d) {return d.y0})
         .attr('width', function (d) { return d.x1 - d.x0; })
         .attr('height', function (d) { return d.y1 - d.y0; })
-        .style("stroke", "black")
-        .style("fill", "slateblue");
+        .style("stroke", "white")
+        .style("fill", function(d) {
+           // console.log("this is d " + d);
+           // console.log(d);
+          // console.log(d.data.category);
+            
+            if(d.data.category == "PC") {
+                console.log("founc pc game" + d.data.name);
+                return "red";
+            }
+            else {
+                return "blue";
+            }
+        });
 }
 
 
